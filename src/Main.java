c98d6051474a490026d7c412c9f2c40ed291da56
Vera Sorokina <vera.sorokina.2013@mail.ru>
2022-10-28 15:54:03 +0400

    A

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        int[] sales = {40, 3, 589, 44 };
+        SalesManager manager = new SalesManager(sales);
+        System.out.println("Макс: " + manager.max());
+    }
 }
